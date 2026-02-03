4c7a0bb5414c6cf1aac86df24bb0f065ce062ab1
s223535784 <s223535784@deakin.edu.au>
2026-02-03 07:38:10 +0530

    Configuring swagger load on app start

--- start.bat
+++ start.bat
@@ -7,6 +7,16 @@ for /f "tokens=5" %%a in ('netstat -aon ^| findstr :8080 ^| findstr LISTENING 2^
 )
 
 echo Port 8080 is free. Starting application...
+echo Swagger UI will open automatically when app is ready...
 echo.
 
-mvn clean spring-boot:run
+start "EatClub API" cmd /c "mvn clean spring-boot:run"
+
+echo Waiting for application to start...
+:waitloop
+timeout /t 2 /nobreak >nul
+curl -s -o nul http://localhost:8080/swagger-ui.html
+if %errorlevel% neq 0 goto waitloop
+
+echo Application started! Opening Swagger UI...
+start "" http://localhost:8080/swagger-ui.html
